76ea1e5092c5996b95573a136a6aa58bb7ffe3fd
Gayun00 <gayunlee11@gmail.com>
2024-02-16 15:33:11 +0900

    Add: 할일 검색 기능

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect } from "react";
 import TodoComposer from "./components/TodoComposer";
 import TodoList from "./components/TodoList";
 import "./globals.css";
@@ -10,19 +10,30 @@ function App() {
   // api 호출을 통해 데이터를 불러온 것으로 가정
   const [todos, setTodos] = React.useState(todoList);
   const [selectedStatus, setSelectedStatus] = React.useState<Status>("todo");
+  const [search, setSearch] = React.useState("");
 
   const selectStatus = (status: Status) => {
     setSelectedStatus(status);
   };
 
-  const getTodoList = (status: Status) => {
-    const filtered = todoList.filter((todo) => todo.status === status);
-    setTodos(filtered);
+  const handleSearch = (text: string) => {
+    setSearch(text);
   };
+
+  const getTodoList = useCallback(
+    (status: Status) => {
+      const filtered = todoList.filter(
+        (todo) => todo.status === status && todo.text.includes(search)
+      );
+
+      setTodos(filtered);
+    },
+    [search]
+  );
 
   useEffect(() => {
     getTodoList(selectedStatus);
-  }, [selectedStatus]);
+  }, [getTodoList, selectedStatus]);
 
   return (
     <div className="mt-20 flex flex-col items-center gap-y-10">
@@ -30,6 +41,7 @@ function App() {
       <TodoFileter
         selectedStatus={selectedStatus}
         selectStatus={selectStatus}
+        handleSearch={handleSearch}
       />
       <TodoList data={todos} />
     </div>
--- src/components/TodoFIlter/index.tsx
+++ src/components/TodoFIlter/index.tsx
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { ChangeEvent } from "react";
 import { Button } from "../ui/button";
 import { Status } from "@/types";
+import { Input } from "../ui/input";
 
 interface Props {
   selectedStatus: Status;
   selectStatus: (status: Status) => void;
+  handleSearch: (text: string) => void;
 }
 
 const statusList: Status[] = ["complete", "todo"];
 
-const TodoFileter = ({ selectedStatus, selectStatus }: Props) => {
+const TodoFileter = ({ selectedStatus, selectStatus, handleSearch }: Props) => {
+  const [keyword, setKeyword] = React.useState("");
+  const handleChangeKeyword = (e: ChangeEvent<HTMLInputElement>) => {
+    setKeyword(e.target.value);
+  };
+
+  const handleClickSearch = () => {
+    handleSearch(keyword);
+  };
+
   return (
-    <div className="space-x-4">
+    <div className="space-x-4 space-y-4">
+      <div className="flex gap-x-4">
+        <Input onChange={handleChangeKeyword} />
+        <Button onClick={handleClickSearch}>검색</Button>
+      </div>
+
       {statusList.map((status) => (
         <Button
           onClick={() => selectStatus(status)}
