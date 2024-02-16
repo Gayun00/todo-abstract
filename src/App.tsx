5ebc51e46e15ecf9f7b69838d36e69394b4efa9b
Gayun00 <gayunlee11@gmail.com>
2024-02-16 15:08:15 +0900

    Add: 할 일 조회 및 추가 로직

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
+import React, { useEffect } from "react";
 import TodoComposer from "./components/TodoComposer";
 import TodoList from "./components/TodoList";
 import "./globals.css";
 import { todoList } from "./mocks";
 
 function App() {
+  const [todos, setTodos] = React.useState(todoList);
+
+  const getTodoList = () => {
+    setTodos(todoList);
+  };
+
+  useEffect(() => {
+    getTodoList();
+  }, []);
+
   return (
     <div className="mt-20 flex flex-col items-center gap-y-10">
       <TodoComposer />
-      <TodoList data={todoList} />
+      <TodoList data={todos} />
     </div>
   );
 }
--- src/components/TodoComposer/index.tsx
+++ src/components/TodoComposer/index.tsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { ChangeEvent } from "react";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 
 const TodoComposer = () => {
+  const [input, setInput] = React.useState("");
+  const onChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setInput(value);
+  };
+
+  const addTodo = () => {
+    console.log("add todo input: ", input);
+  };
+
   return (
     <div className="flex w-full max-w-sm items-center space-x-2">
-      <Input />
-      <Button>입력</Button>
+      <Input onChange={onChangeInput} />
+      <Button onClick={addTodo}>입력</Button>
     </div>
   );
 };
